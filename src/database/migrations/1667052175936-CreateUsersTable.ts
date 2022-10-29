f4b6c1ca3d5d16fa6fd4a7401e5adb7539c5d0f0
joelvitorniino <joelprofessor13@gmail.com>
2022-10-29 11:08:57 -0300

    created migration of create users table

---
+++ src/database/migrations/1667052175936-CreateUsersTable.ts
@@ -0,0 +1,34 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class CreateUsersTable1667052175936 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"');
+
+    await queryRunner.createTable(new Table({
+        name: 'users',
+        columns: [
+            {
+                name: 'id',
+                type: 'uuid',
+                isPrimary: true,
+                generationStrategy: 'uuid',
+                default: 'uuid_generate_v4()',
+            },
+            {
+                name: 'email',
+                type: 'varchar',
+                isUnique: true,
+            },
+            {
+                name: 'password',
+                type: 'varchar',
+            }
+        ],
+    }))
+  };
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('users');
+    await queryRunner.query('DROP EXTENSION "uuid-ossp"');
+  };
+};
