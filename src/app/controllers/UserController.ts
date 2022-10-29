b1dca2477d7225636fb4b94a347b8dcbd9ba269c
joelvitorniino <joelprofessor13@gmail.com>
2022-10-29 11:51:59 -0300

    post user

---
+++ src/app/controllers/UserController.ts
@@ -0,0 +1,23 @@
+import { Request, Response } from "express";
+import { AppDataSource } from "../../database/data-source";
+import { User } from "../models/User";
+
+class UserController {
+    async store(request: Request, response: Response) {
+        const repository = AppDataSource.getRepository(User);
+        const { email, password } = request.body;
+
+        const userExists = await repository.findOne({ where:{ email } });
+
+        if(userExists) {
+            return response.sendStatus(409);
+        };
+
+        const user = repository.create({ email, password });
+        await repository.save(user);
+
+        return response.json(user);
+    };
+};
+
+export default new UserController();
--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,9 @@
 import { Router } from 'express';
 
+import UserController from './app/controllers/UserController';
+
 const router = Router();
+
+router.post('/users', UserController.store);
 
 export default router;
